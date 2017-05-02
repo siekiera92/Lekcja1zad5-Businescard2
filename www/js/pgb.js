c4fcd7814f9f004b4bf7884e5fbc83fd2a1fc8ba
siekiera92 <piotr.siekierski.92@gmail.com>
2017-05-02 12:18:57 +0200

    1

---
+++ www/js/pgb.js
@@ -0,0 +1,33 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function myInfo() {
+
+	info =  'Jan Kowalski' + '\n' +
+			'Kraków' + '\n' +
+			'ul. Zajebistości 1/1' + '\n' + 
+			'aaa@aaa.pl: ' + '\n';
+
+	navigator.notification.alert(info);
+	
+}
